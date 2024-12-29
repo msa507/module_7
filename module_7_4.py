dce26f2fcefda479f199f6c5c4fa219b8436a84e
msa507 <msa10101962@gmail.com>
2024-12-29 11:07:01 +0500

    Add files via upload

---
+++ module_7_4.py
@@ -0,0 +1,32 @@
+# Использование %
+team1_num = 5
+team2_num = 6
+print('В команде Мастера кода участников: %s !' % team1_num)
+print('Итого сегодня в командах участников: %s и %s !' % (team1_num, team2_num))
+
+# Использование format()
+score_1 = 40
+score_2 = 42
+print('Команда Волшебники данных решила задач: {} !'.format(score_2))
+
+team1_time = 18015.2
+team2_time = 2153.31451
+print(' Волшебники данных решили задачи за {} с !'.format(team1_time))
+
+# Использование f-строк:
+
+print(f'Команды решили {score_1} и {score_2} задачи.')
+
+if score_1 > score_2 or score_1 == score_2 and team1_time > team2_time:
+    result = 'Победа команды Мастера кода!'
+elif score_1 < score_2 or score_1 == score_2 and team1_time < team2_time:
+    result = 'Победа команды Волшебники Данных!'
+else:
+    result = 'Ничья!'
+challenge_result = result
+print(f'Результат битвы: {challenge_result}')
+
+tasks_total = score_1 + score_2
+time_avg = (team2_time + team1_time) / tasks_total
+print(f'Сегодня было решено {tasks_total} задач, в среднем по {time_avg} секунды на задачу!.')
+
